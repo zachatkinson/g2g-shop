60d37ef822d9b4097a388756c2a6e383787d0a0c
Zach Atkinson <zach.atkinson85@gmail.com>
2022-06-12 13:30:52 -0400

    FAQ Content added for products

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -23,7 +23,7 @@ export default function FaqPage({data}) {
                         <AccordionItemButton className={styles.accordion__button}>Why do you use pressurized wood?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>Good2Grow planters are constructed with pressurized wood because of its strength and durability making it ideal for standing up to the rigors of the weather and elements. Additionally, pressurized wood is easy to maintain and can be cleaned with soap and water. Pressurized wood is also an environmentally friendly material that can be recycled or reused.</p>
                     </AccordionItemPanel>
                 </AccordionItem>
 
@@ -32,16 +32,16 @@ export default function FaqPage({data}) {
                         <AccordionItemButton className={styles.accordion__button}>Is your product locally sourced?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>All Good2Grow planters are constructed using high-quality, pressure treated Canadian lumber.</p>
                     </AccordionItemPanel>
                 </AccordionItem>
 
                 <AccordionItem className={styles.accordion__item}>
                     <AccordionItemHeading>
-                        <AccordionItemButton className={styles.accordion__button}>What can I plant in Good2Grow gardens?</AccordionItemButton>
+                        <AccordionItemButton className={styles.accordion__button}>What can I plant in Good2Grow planters?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>Flowers, shrubs, vegetables, and more! Due to our generous soil depth, you can grow anything you like in our planters.</p>
                     </AccordionItemPanel>
                 </AccordionItem>
 
@@ -50,7 +50,7 @@ export default function FaqPage({data}) {
                         <AccordionItemButton className={styles.accordion__button}>What maintenance is required?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>When your Good2Grow planter arrives, you'll fill it with soil, add your plants, and hook up your water supply.  Afterwards, the only maintenance required will be harvesting and (optionaally) cleaning the exterior of the box come planting season using soap and water.</p>
                     </AccordionItemPanel>
                 </AccordionItem>
 
@@ -59,7 +59,7 @@ export default function FaqPage({data}) {
                         <AccordionItemButton className={styles.accordion__button}>What customizable elements can be added to your planters?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>If you're looking for something custom, our craftsmen are standing by and ready to help. Head over to our custom builds page now to get started on your project.</p>
                     </AccordionItemPanel>
                 </AccordionItem>
 
@@ -68,7 +68,7 @@ export default function FaqPage({data}) {
                         <AccordionItemButton className={styles.accordion__button}>How much assembly is required?</AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel className={styles.accordion__panel}>
-                        <p>This is some text.</p>
+                        <p>All Good2Grow planters are delivered fully constructed. Simply add your soil, plants, and water connection!</p>
                     </AccordionItemPanel>
                 </AccordionItem>
             </Accordion>
