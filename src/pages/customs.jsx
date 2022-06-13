a3911dfc4063b9222eea6790c9ecc74f489e5871
Zach Atkinson <zach.atkinson85@gmail.com>
2022-06-13 12:34:55 -0400

    tweaks to autocomplete to target north america

--- src/pages/customs.jsx
+++ src/pages/customs.jsx
@@ -39,7 +39,7 @@ export default function CustomPage({data}) {
                         }}
                         options={{
                             types: ["(regions)"],
-                            componentRestrictions: { country: " ca" },
+                            componentRestrictions: { country: "ca" },
                         }}
                     />
 
