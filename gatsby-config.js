1f0c18621ddb999fbc189f73159fdb20c3ab9c5d
Zach Atkinson <zach.atkinson85@gmail.com>
2022-07-06 12:50:56 -0400

    testing purgecss plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,6 +123,17 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+                printRejected: true, // Print removed selectors and processed file names
+                // develop: true, // Enable while using `gatsby develop`
+                // tailwind: false, // Enable tailwindcss support
+                // whitelist: ['whitelist'], // Don't remove this selector
+                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+                // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+            }
+        },
         "gatsby-remark-images",
         "gatsby-transformer-remark",
         "gatsby-plugin-image",
